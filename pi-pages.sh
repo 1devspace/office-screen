7d0ecd59e3bf61d98a45a32ef7650b1e5b8d8eaa
Marouen Helali <marwanest@live.fr>
2024-05-08 15:10:18 -0700

    add command and sleep

--- pi-pages.sh
+++ pi-pages.sh
@@ -1,1 +1,22 @@
-echo "Running pi-pages...:
+#!/bin/bash
+
+echo "Running pi-pages..."
+
+
+# List of websites to cycle through
+websites=("https://news.ycombinator.com/" "https://www.coin360.com")
+
+# Time interval to switch between tabs (in seconds)
+interval=60
+
+# Loop infinitely
+while true; do
+    # Loop through each website in the array
+    for website in "${websites[@]}"; do
+        # Open Chromium browser with the specified website in fullscreen mode
+        DISPLAY=:0 chromium-browser "$website" --start-fullscreen
+        
+        # Sleep for the specified interval
+        sleep $interval
+    done
+done
