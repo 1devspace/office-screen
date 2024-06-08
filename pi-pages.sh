02e50b17a3af8e83113e46b042d1d0b366701fbb
Marouen Helali <marwanest@live.fr>
2024-06-07 21:21:08 -0700

    add links

--- pi-pages.sh
+++ pi-pages.sh
@@ -2,19 +2,39 @@
 
 echo "Running pi-pages..."
 
-
-# List of websites to cycle through
-websites=("https://news.ycombinator.com/" "https://www.coin360.com")
+# List of URLs to visit
+urls=(
+    "https://wise.com/us/currency-converter/usd-to-tnd-rate"
+    "https://www.mortgagenewsdaily.com/mortgage-rates/mnd"
+    "https://www.forbes.com/advisor/mortgages/mortgage-rates/"
+    "https://finance.yahoo.com/most-active/"
+    "https://finance.yahoo.com/quote/CNC/"
+    "https://www.coindesk.com/price/bitcoin/"
+    "https://www.livesport.com/soccer/world/"
+    "https://live-tennis.eu/en/atp-live-scores"
+    "https://forecast.weather.gov/MapClick.php?CityName=Las+Vegas&state=NV&site=VEF&textField1=36.175&textField2=-115.136&e=0"
+    "https://www.weatherbug.com/weather-forecast/10-day-weather/tabarka-jundubah-ts"
+    "https://tldr.tech/"
+    "https://news.ycombinator.com/"
+    "https://gov.nv.gov/Newsroom/PRs/news-releases/"
+    "https://cityofnorthlasvegas.org/newslist.php"
+    "https://www.lasvegasnevada.gov/Residents/Events#/"
+    "https://www.cityofnorthlasvegas.com/things-to-do/events-calendar"
+    "https://github.com/trending"
+    "https://trends24.in/united-states/"
+)
 
 # Time interval to switch between tabs (in seconds)
-interval=60
+interval=10
 
 # Loop infinitely
 while true; do
-    # Loop through each website in the array
-    for website in "${websites[@]}"; do
-        # Open Chromium browser with the specified website in fullscreen mode
-        DISPLAY=:0 chromium-browser "$website" --start-fullscreen
+    # Loop through each URL in the array
+    for url in "${urls[@]}"; do
+        # Open Chromium browser with the specified website
+        # --disable-gpu is used to disable GPU hardware acceleration
+        # --window-size sets the initial window size
+        chromium-browser "$url" --window-size=1920,1080
         
         # Sleep for the specified interval
         sleep $interval
